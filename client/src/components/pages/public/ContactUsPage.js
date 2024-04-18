e7d451255c916060313e5c889b77a5ac61e5b18c
AnselNolting <anselnolting@gmail.com>
2024-04-17 20:14:10 -0400

    n/a

--- client/src/components/pages/public/ContactUsPage.js
+++ client/src/components/pages/public/ContactUsPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+
 export default function ContactUsPage () {
   return (
     <div className = "main-content">
       <h1>Welcome to the "contact us" page!</h1>
       <p>Welcome to the "contact us" page!</p>
+  
     </div>
   );
 };
