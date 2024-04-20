e68cba0df2c9fb0be0f62665c0690357b3ed0690
Richard Gullo <57539853+Xylloz@users.noreply.github.com>
2024-04-20 15:00:40 -0400

    added img fetching on client

--- client/src/pages/Plants/PlantCard.js
+++ client/src/pages/Plants/PlantCard.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const PlantCard = ({ plant }) => {
-  console.log('plant: ', plant);
+  console.log("plant: ", plant);
   return (
     <div className="bg-white/90 max-w-md mx-auto rounded overflow-hidden shadow-md m-4">
-      {/* <img
+      <img
         className="w-full h-64 object-cover"
-        src={plant.img}
+        src={`http://localhost:3000/images/${plant.plant_filename}`}
         alt={plant.name}
-      /> */}
+      />
       <div className="px-6 py-4">
         <div
           className={`font-bold text-xl mb-2 ${
-            plant.fav_flag ? 'text-yellow-300' : 'text-gray-700'
+            plant.fav_flag ? "text-yellow-300" : "text-gray-700"
           }`}
         >
           {plant.plantname}
--- server/controllers/plantController.js
+++ server/controllers/plantController.js
@@ -1,28 +1,41 @@
-const db = require('../models/queryModels');
-const plantController ={};
+const db = require("../models/queryModels");
+const plantController = {};
 
-plantController.createPlant = async(req, res, next) => {
-   const {plantname, wateringInstruction,fertilizeInstruction,sunlight, image} = req.body;
-   console.log(plantname)
-   console.log(wateringInstruction)
-   try{
-     const text = `INSERT INTO plants (plantname, watering_instruction,fertilize_instruction,sunlight, image)
+plantController.createPlant = async (req, res, next) => {
+  const {
+    plantname,
+    wateringInstruction,
+    fertilizeInstruction,
+    sunlight,
+    image,
+  } = req.body;
+  console.log("started plantController.createPlant");
+  console.log("createPlant: ", plantname);
+  try {
+    const text = `INSERT INTO plants (plantname, watering_instruction,fertilize_instruction,sunlight, image)
        VALUES ($1,$2,$3,$4,$5)
        RETURNING *`;
-     const params = [plantname, wateringInstruction,fertilizeInstruction,sunlight, image];
-     const result = await db.query(text,params);
-     console.log(result)
-     if(result.rowCount!==1){
+    const params = [
+      plantname,
+      wateringInstruction,
+      fertilizeInstruction,
+      sunlight,
+      image,
+    ];
+    const result = await db.query(text, params);
+    console.log(result);
+    if (result.rowCount !== 1) {
       throw err;
-     }
-     res.locals.plant = result.rows[0];
-     console.log(res.locals.user)
-     return next();
-   }
-   catch (err) {
-      console.error('Error in userControllersignupUser:', err);
-      return res.status(500).json({ error: 'userControllersignupUser error' });
-     }
-}
+    }
+    res.locals.plant = result.rows[0];
+    console.log("successfully created plant: ", res.locals.plant);
+    return next();
+  } catch (err) {
+    console.error("Error in plantController.createPlant: ", err);
+    return res
+      .status(500)
+      .json({ error: "plantController.createPlant: error" });
+  }
+};
 
 module.exports = plantController;
